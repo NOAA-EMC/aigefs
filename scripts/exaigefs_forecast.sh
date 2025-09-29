6cc778570c6325e5ae6ade17344a42ad85485c34
Russell Manser <russell.manser@noaa.gov>
2025-09-29 16:50:38 +0000

    Shared tmp dir, update COM paths, use mem weights

--- scripts/exaigefs_forecast.sh
+++ scripts/exaigefs_forecast.sh
@@ -4,38 +4,22 @@ set -x
 echo "Starting $0"
 
 mkdir -p ${DATA}/data
+mkdir -p ${DATAshared}/output
 
 # copy data in
 cpreq $COMIN/aigefs.${cycle}.ic.nc $DATA/data
 ls -l $DATA/data
 
+mem_num=$( echo $MEMBER | egrep -o "[0-9]+" | bc )
+
 # run graphcast forecast
 $USHaigefs/run_graphcast.py \
+	-n ai${GEFS_MEMBER} \
 	-i ${DATA}/data/aigefs.${cycle}.ic.nc \
-	-n ml${GEFS_MEMBER} \
-	-o ${DATA} \
+	-o ${DATAshared}/output \
 	-w $FIXaigefs \
-	-l 64
+	-l 64 \
+        -c ${FIXaigefs}/ens_weights/member${mem_num}.pkl
 export err=$?; err_chk
-
-for fh in $( seq -f "%03g" 0 6 384 ); do
-  file=$COMOUT/aigefs.${cycle}.pres.f${fh}.grib2
-  cpfs mlgefs.${cycle}.pres.0p25.f${fh}.grib2 ${file}
-  cpfs mlgefs.${cycle}.pres.0p25.f${fh}.grib2.idx ${file}.idx
-
-  if [ "$SENDDBN" = "YES" ]; then
-    $DBNROOT/bin/dbn_alert MODEL AIGEFS_GB2 $job ${file}
-    $DBNROOT/bin/dbn_alert MODEL AIGEFS_GB2_IDX $job ${file}.idx
-  fi
-
-  file=$COMOUT/aigefs.${cycle}.sfc.f${fh}.grib2
-  cpfs mlgefs.${cycle}.sfc.0p25.f${fh}.grib2 ${file}
-  cpfs mlgefs.${cycle}.sfc.0p25.f${fh}.grib2.idx ${file}.idx
-
-  if [ "$SENDDBN" = "YES" ]; then
-    $DBNROOT/bin/dbn_alert MODEL AIGEFS_GB2 $job ${file}
-    $DBNROOT/bin/dbn_alert MODEL AIGEFS_GB2_IDX $job ${file}.idx
-  fi
-done
 
 echo "$0 completed normally"
